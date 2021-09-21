d51fb56d9791bcdea3f294539dd6c215d1c717f7
Kevin Z <kjzhou2@illinois.edu>
2021-09-21 13:05:25 -0500

    clapper with extra reverse step

--- 5_clapper/clapper.py
+++ 5_clapper/clapper.py
@@ -1,11 +1,28 @@
 
-def clapper(trope, repeat, wraparound=False):
-    pass
+def clapper(trope, repeat, reverse=False, wraparound=False):
+    index = 0
+    offset = 0
+    wraps = len(trope)
 
+    if wraparound:
+        wraps += 1
+    for j in range(wraps):
+        for k in range(len(trope)):
+            for i in range(repeat):
+                layer = []
+                layer.append(trope[(index % len(trope))])
+                layer.append(trope[(index + offset) % len(trope)])
+
+                yield layer
+                index += 1
+        if reverse:
+            offset -= 1
+        else:
+            offset += 1
 
 if __name__ == '__main__':
 
-    print([x for x in clapper([1,2,3], 1)])
+    print([x for x in clapper([1,2,3], 1, True, True)])
 
     print([x for x in clapper("ABCD", 2, True)])
 
